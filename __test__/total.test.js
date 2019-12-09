2b1d06ab900e2a36600de8de0649a33dd206e423
evanandrewson <evanandrewson@gmail.com>
2019-12-09 11:00:57 -0800

    made total post route

---
+++ __test__/total.test.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+
+describe('total routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can post a total', () => {
+    const agent = request.agent(app);
+    return agent
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test1', password: 'abc1' })
+      .then(user => {
+        return agent
+          .post('/api/v1/total')
+          .send({
+            totals: [{ name: 'USD', amount: 5000 }, { name: 'Bitcoin', amount: 1 }]
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              totals: [
+                {
+                  name: 'USD',
+                  amount: 5000
+                },
+                {
+                  name: 'Bitcoin',
+                  amount: 1
+                }
+              ],
+              user: expect.any(String),
+              __v: 0,
+              timestamp: expect.any(String),
+              _id: expect.any(String)
+            });
+          });
+      });
+  });
+});
