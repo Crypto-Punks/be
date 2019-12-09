f0e69757504b29912f03fe65c8831390b33b5d2c
evanandrewson <evanandrewson@gmail.com>
2019-12-09 11:08:16 -0800

    made get all route for total

--- __test__/total.test.js
+++ __test__/total.test.js
@@ -23,7 +23,7 @@ describe('total routes', () => {
     return agent
       .post('/api/v1/auth/signup')
       .send({ username: 'test1', password: 'abc1' })
-      .then(user => {
+      .then(() => {
         return agent
           .post('/api/v1/total')
           .send({
@@ -46,6 +46,42 @@ describe('total routes', () => {
               timestamp: expect.any(String),
               _id: expect.any(String)
             });
+          });
+      });
+  });
+
+  it('can get all totals for a user', () => {
+    const agent = request.agent(app);
+    return agent
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test1', password: 'abc1' })
+      .then(() => {
+        return agent
+          .post('/api/v1/total')
+          .send({
+            totals: [{ name: 'USD', amount: 5000 }, { name: 'Bitcoin', amount: 1 }]
+          })
+          .then(() => {
+            return agent
+              .get('/api/v1/total')
+              .then(res => {
+                expect(res.body).toEqual([{
+                  totals: [
+                    {
+                      name: 'USD',
+                      amount: 5000
+                    },
+                    {
+                      name: 'Bitcoin',
+                      amount: 1
+                    }
+                  ],
+                  user: expect.any(String),
+                  __v: 0,
+                  timestamp: expect.any(String),
+                  _id: expect.any(String)
+                }]);
+              });
           });
       });
   });
