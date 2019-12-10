85fc69c209079a3945d51f03215a802308283450
Joe Klause <joeklause@gmail.com>
2019-12-10 07:16:42 -0800

    removed old code

--- get-data/combinedData.js
+++ get-data/combinedData.js
@@ -2,27 +2,6 @@ const marketCapData = require('./coinCapMarket');
 const coinCapData = require('./coinCap');
 const fs = require('fs');
 
-
-// const combinedData = marketCapData.map(mcData => {
-//   return coinCapData.reduce((acc, ccData) => {
-//     if(mcData.symbol === ccData.symbol) {
-//       acc.id = ccData.id,
-//       acc.symbol = ccData.symbol,
-//       acc.description = mcData.description;
-//       acc.website = mcData.urls.website[0];
-//       acc.logo = mcData.logo;
-//     }
-//     return acc;
-//   }, {});
-// })
-//   .reduce((arr, obj) => {
-
-//     if(obj.id) {
-//       arr.push(obj);
-//     }
-//     return arr;
-//   }, []);
-
 const combinedData = marketCapData.map(mcData => {
   const ccData = coinCapData.find(element => mcData.symbol === element.symbol);
   if(ccData) {
@@ -39,8 +18,6 @@ const combinedData = marketCapData.map(mcData => {
     if(obj) arr.push(obj);
     return arr;
   }, []);
-
-
 
 
 fs.writeFileSync('json-data.json', JSON.stringify(combinedData, true, 2));
